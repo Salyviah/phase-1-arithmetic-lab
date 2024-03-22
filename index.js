799541d9de00237efde007efec11cefdc01cac10
salyviah <salyviahkamau@gmail.com>
2024-03-22 07:51:44 +0300

    phase-1-arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 31, num2 = 2
+const multiply = num1*num2
+
+const random = 2
+random >= 0
+
+let num3 = 8, num4 = 2
+const mod = num3 / num4
+
+
+let numbers = [7, 10, 20, 15, 7]
+
+let max = Math.max(...numbers)
+
 
 // Write your code here
